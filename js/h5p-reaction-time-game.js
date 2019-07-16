318a949aa1860aea609a60813161fa7a671198d4
sherin-shifana <sherinshifana1997@gmail.com>
2019-07-16 17:44:10 +0530

    commit

--- js/h5p-reaction-time-game.js
+++ js/h5p-reaction-time-game.js
@@ -6,104 +6,20 @@ H5P.ReactionTimeGame = (function ($, UI) {
     // Keep provided id.
     that.id = id;
   }
+
   ReactionTimeGame.prototype.attach = function ($container) {
     const that=this;
-    $container.css('height', $(window).width()/2.5);
-    const $mainContainer = $('<div class="main-container"></div>');
+    $container.addClass('reaction-time-game');
+    const $wrapper = $('<div class="wrapper"></div>');
+    const $shape = $('<p>Shape:&nbsp;'+this.options.shape+'</p>');
+    const $color = $('<p>Color:&nbsp'+this.options.color+'</p>');
+    const $taskDescription = $('<div class="task-description">'+this.options.taskDescription+'<br/></div>');
     const $startButton = $('<button class="start-button">Start</button>');
-    $startButton.click(function() {
-      $(this).remove();
-      $('<canvas id="shape"></canvas>').appendTo($mainContainer);
-      const canvasElement = $("#shape");
-      const ctx = canvasElement[0].getContext("2d");
-      var isEmpty = function(ctx){
-        var data = ctx.getImageData(0,0,ctx.canvas.width, ctx.canvas.height).data;
-        return !Array.prototype.some.call(data, function(p){return p>0;});
-      }
-      // console.log(isEmpty(ctx));
-      // console.log(setInterval(that.getRandomColor, 1000));
-      setInterval(function(){
-        console.log($("#shape").length);
-        if(isEmpty(ctx)) {
-          $("#shape").append(that.getRandomShape());
-        }
-        else {
-          that.clearCanvas(ctx);
-        }
-
-      }, 1000);
-    });
-    $startButton.appendTo($mainContainer);
-    $mainContainer.appendTo($container);
-  };
-
-  ReactionTimeGame.prototype.clearCanvas = function(ctx){
-    let canvas = $("#shape");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
-
-  ReactionTimeGame.prototype.getRandomShape = function () {
-    const that = this;
-    const shapes = [that.square, that.triangle, that.circle];
-    for (var i = 0; i < shapes.length; i++) {
-        shape = shapes[Math.round(Math.random() * i)];
-    }
-    return shape;
-  };
-
-  ReactionTimeGame.prototype.getRandomColor = function ()  {
-
-    var colors = ["red", "green", "blue", "black", "brown"];
-    var color;
-    for (var i = 0; i < colors.length; i++) {
-        color = colors[Math.round(Math.random() * i)];
-    }
-    return color;
-  }
-
-  ReactionTimeGame.prototype.setRandomColor = function() {
-    const that = this;
-    const canvasElement = $("#shape");
-    const ctx = canvasElement[0].getContext("2d");
-    console.log(that.getRandomColor());
-    ctx.fillStyle = that.getRandomColor();
-    ctx.fill();
-  }
-
-  ReactionTimeGame.prototype.triangle = function() {
-    const that = this;
-    const canvasElement = $("#shape");
-    const ctx = canvasElement[0].getContext("2d");
-    ctx.beginPath();
-    setInterval(that.setRandomColor, 1000);
-    ctx.moveTo(150, 50);
-    ctx.lineTo(150 + (80 / 2), 50 + 80);
-    ctx.lineTo(150 - (80 / 2), 50 + 80);
-    ctx.closePath();
-    ctx.stroke();
-
-  }
-
-  ReactionTimeGame.prototype.square = function() {
-    const that = this;
-    const canvasElement = $("#shape");
-    const ctx = canvasElement[0].getContext("2d");
-    ctx.rect(100, 20, 80, 80);
-    setInterval(that.setRandomColor, 1000);
-    ctx.stroke();
-  }
-
-  ReactionTimeGame.prototype.circle = function() {
-    const that = this;
-    const canvasElement = $("#shape");
-    const ctx = canvasElement[0].getContext("2d");
-    setInterval(that.setRandomColor, 1000);
-    ctx.beginPath();
-    ctx.arc(150, 80, 50, 0, 2 * Math.PI);
-
-    ctx.stroke();
+    $shape.appendTo($taskDescription);
+    $color.appendTo($taskDescription);
+    $taskDescription.appendTo($wrapper);
+    $startButton.appendTo($wrapper);
+    $wrapper.appendTo($container);
   }
 
   return ReactionTimeGame;
